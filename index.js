a988dd9132d18668365bac6d8071252c570ca2d9
Movsar Ma <melkoto@yandex.ru>
2022-08-16 19:01:03 +0300

    js

---
+++ index.js
@@ -0,0 +1,22 @@
+let num1 = 1 // number
+let num2 = 2.4 // number
+let num3 = -23
+
+let str1 = "My name is Mo" // string
+let str2 = 'My name is Mozvar'
+let str3 = `My name is Mosvar`
+
+//          0 1 2 3 4  5  6
+let nums = [1,2,3,4,5,-10,2]
+//              0      1       2      3
+let names = ["Adam", "Ali", "Timur", "Mo"]
+
+// создайте массив имен
+// создайте массив типа ["Имя", возраст] ["Movsar", 32, "Adam", 27 ...]
+// выведите в консоль 
+// Имя: Мовсар, Возраст: 32
+let nameAge = ["Movsar", 32, "Ali", 29] // 5 имя 5 возраст
+
+console.log("Имя:", nameAge[0], ",", "Возраст:", nameAge[1]);
+console.log("Имя:", nameAge[2], ",", "Возраст:", nameAge[3]);
+
